f4edae04499fcab67198dcbf0428bda61192a203
EC2 Default User <ec2-user@ip-10-16-10-192.us-west-2.compute.internal>
2024-08-09 14:20:34 +0000

    lab day1

---
+++ lab2.py
@@ -0,0 +1,36 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue)+" is of the data type "+ str(type(myValue)))
+
+print("==== Ex.3 ===================")
+myValue=3.14
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue)+" is of the data type "+ str(type(myValue)))
+
+print("==== Ex.4 ===================")
+myValue = 5j
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue)+" is of the data type "+ str(type(myValue)))
+
+print("==== Ex.5 ===================")
+myValue = False 
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue)+" is of the data type "+ str(type(myValue)))
